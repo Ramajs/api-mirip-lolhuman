88e11cbbf5799ce2aac6d0c340a70f272342485b
Ramajs <109258889+Ramajs@users.noreply.github.com>
2022-12-26 14:32:12 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'Ramaa-gnnz' 
+global.apikey = ["rama", "RamaaGnnz", "reyhan"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
